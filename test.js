c20ed46fcddd77d43eed1b89d934636ecb9af6dd
jonschlinkert <jon.schlinkert@sellside.com>
2016-03-25 13:40:33 -0400

    update tests

--- test.js
+++ test.js
@@ -7,7 +7,7 @@ var globKeys = require('./');
 describe('globKeys', function() {
   it('should create an array of keys from a glob of files', function() {
     var keys = globKeys('fixtures/*.js');
-    assert.deepEqual(keys, ['bar', 'baz', 'qux', 'fez', 'foo']);
+    assert.deepEqual(keys.sort(), ['bar', 'baz', 'qux', 'fez', 'foo', 'abc', 'xyz'].sort());
   });
 
   it('should not choke when no keys exist', function() {
